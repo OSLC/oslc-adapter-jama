843888e28707c268213a8177889d4ee045b9c061
Axel Reichwein <axel.reichwein@koneksys.com>
2018-06-25 21:33:25 -0700

    fixed user authorization handing out access token by checking the user credentials according to username an pwd, instead of consumer key and secret (consumer key and secret are used to get the request token)

--- src/main/java/com/jama/oslc/web/JamaCredentialsFilter.java
+++ src/main/java/com/jama/oslc/web/JamaCredentialsFilter.java
@@ -4,6 +4,10 @@ import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 import org.eclipse.lyo.server.oauth.consumerstore.FileSystemConsumerStore;
 import org.eclipse.lyo.server.oauth.core.AuthenticationException;
@@ -54,21 +58,37 @@ public class JamaCredentialsFilter extends AbstractAdapterCredentialsFilter2 {
 		if (credentialsObject instanceof Credentials) {
 			Credentials credentials = (Credentials) credentialsObject;
 			try {
-//				FileSystemConsumerStore fileSystemConsumerStore = new FileSystemConsumerStore("jamaOAuthStore.xml");
-				FileSystemConsumerStore fileSystemConsumerStore = new FileSystemConsumerStore(AdapterInitializer.localConsumerStoreLocation);
-				LyoOAuthConsumer consumer = fileSystemConsumerStore.getConsumer(credentials.getUsername());
-				if (consumer == null) {
+
+				String usernameAndPassword = credentials.getUsername() + ":" + credentials.getPassword();
+				String authorizationHeaderValue = "Basic "
+						+ java.util.Base64.getEncoder().encodeToString(usernameAndPassword.getBytes());
+				Client client = ClientBuilder.newClient();
+				WebTarget myResource = client.target("https://" + AdapterInitializer.jamaInstanceName + ".jamacloud.com/rest/latest/projects");
+				int responseStatus = myResource.request(MediaType.APPLICATION_JSON)
+						.header("Authorization", authorizationHeaderValue).get().getStatus();
+				if (responseStatus != 200) {
 					throw new com.jama.oslc.web.UnauthorizedException();
 				}
-			} catch (ConsumerStoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+								
+//				FileSystemConsumerStore fileSystemConsumerStore = new FileSystemConsumerStore(AdapterInitializer.localConsumerStoreLocation);
+//				LyoOAuthConsumer consumer = fileSystemConsumerStore.getConsumer(credentials.getUsername());
+//				if (consumer == null) {
+//					throw new com.jama.oslc.web.UnauthorizedException();
+//				}
+			} 
+			
+//			catch (ConsumerStoreException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} catch (ClassNotFoundException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} catch (SQLException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			catch (Exception e) {
+				throw new com.jama.oslc.web.UnauthorizedException();
 			}
 		} else {
 			throw new com.jama.oslc.web.UnauthorizedException();
