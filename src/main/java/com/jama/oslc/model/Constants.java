a6e32083e2a368555701ee98f90f4b7e40307aa9
Axel Reichwein <axel.reichwein@koneksys.com>
2018-07-19 14:19:26 -0700

    adding comment

--- src/main/java/com/jama/oslc/model/Constants.java
+++ src/main/java/com/jama/oslc/model/Constants.java
@@ -19,10 +19,9 @@ import com.jama.oslc.web.AdapterInitializer;
 public class Constants {
 	
 	public static final String domain = "localhost";
-	public static final String portNumber = ":8080";
+	public static final String portNumber = ":8080";	// for no port, or port 80 or 447, just provide empty string ""
 	
-//	 public static final String CONTEXT = "http://localhost:" + AdapterInitializer.portNumber + "/jama-oslc-adapter/";
-//	 public static final String CONTEXT = "http://" + AdapterInitializer.domain + ":" + AdapterInitializer.portNumber + "/jama-oslc-adapter/";
+
 	 public static final String CONTEXT = "http://" + domain + portNumber + "/jama-oslc-adapter/";
 	 public static final String RESOURCES = CONTEXT + "services/";
 	 public static final String VOCABULARY = CONTEXT + "vocabulary/";
