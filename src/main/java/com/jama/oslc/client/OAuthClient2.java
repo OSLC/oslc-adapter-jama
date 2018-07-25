b31b4810d7fc84cb1b23516e4d28ab09c9a91c4a
Axel Reichwein <axel.reichwein@koneksys.com>
2018-07-24 20:36:09 -0700

    client for oauth test on public server

---
+++ src/main/java/com/jama/oslc/client/OAuthClient2.java
@@ -0,0 +1,44 @@
+package com.jama.oslc.client;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.wink.client.ClientResponse;
+import org.eclipse.lyo.client.oslc.OSLCConstants;
+import org.eclipse.lyo.client.oslc.OslcOAuthClient;
+
+import com.jama.oslc.model.Constants;
+import com.jama.oslc.web.AdapterInitializer;
+
+import net.oauth.OAuthException;
+
+public class OAuthClient2 {
+
+	public static void main(String[] args) {
+		String baseUrl = "http://demo.koneksys.com/jama-oslc-adapter/services/";		
+		String consumerKey = "testkey";
+		String consumerSecret = "testsecret";
+		String oauthRealmName = "Jama";
+		
+		OslcOAuthClient oslcOAuthClient = new OslcOAuthClient(baseUrl + "oauth/requestToken", 
+				baseUrl + "oauth/authorize", 
+				baseUrl + "oauth/accessToken", consumerKey, consumerSecret, oauthRealmName);
+
+		try {
+			ClientResponse clientResponse = oslcOAuthClient.getResource("http://demo.koneksys.com/jama-oslc-adapter/services/serviceProviderCatalog", OSLCConstants.CT_RDF);
+			System.out.println(clientResponse.getStatusCode());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
